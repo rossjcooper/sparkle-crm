fc82d3aaddfcec0c308fa1cc9ec9410cf03b68a0
Ross Cooper <therossjcooper@gmail.com>
2016-07-07 23:10:23 +0100

    Initial commit, configuring the HerBert framework

---
+++ app/shortcodes.php
@@ -0,0 +1,3 @@
+<?php namespace SparkleCRM;
+
+/** @var \Herbert\Framework\Shortcode $shortcode */
---
+++ plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Sparkle CRM
+ * Plugin URI:        http://www.ross-cooper.co.uk
+ * Description:       A plugin.
+ * Version:           0.1.0
+ * Author:            Ross Cooper
+ * Author URI:        http://www.ross-cooper.co.uk
+ * License:           MIT
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/getherbert/framework/bootstrap/autoload.php';
